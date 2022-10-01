d4ad4a13f3c9e31fbf9b35eee3bcb321694c055d
Suellen Kathlen Teixeira <99774430+Suellen-Kathlen@users.noreply.github.com>
2022-10-01 19:39:33 -0300

    Add files via upload
    Projeto de bootcamp da DIO

---
+++ br.com.dio.bancodigital/src/br/com/dio/bancodigital/Main.java
@@ -0,0 +1,21 @@
+package br.com.dio.bancodigital;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente();
+		cc.depositar(100);
+		
+		Conta poupanca = new ContaPoupanca();
+		cc.transferir(20, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+	}
+
+}
